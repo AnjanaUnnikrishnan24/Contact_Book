c3029e2b9c8b2232fba070cdfd8d2b5a19aba6bb
AnjanaUnnikrishnan24 <anjana24unni@gmail.com>
2025-04-14 23:13:20 +0530

    initial

---
+++ backend/src/contacts/contact.schema.ts
@@ -0,0 +1,44 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+export type ContactDocument = Contact & Document;
+
+@Schema()
+export class Contact {
+
+    @Prop({ required: true })
+    fullname: string;
+
+    @Prop({ unique:true,required: true })
+    mobilePhone: string;
+
+    @Prop({ unique:true,required: true })
+    lanPhone: string;
+
+    @Prop({unique:true,required: true})
+    email: string;
+
+    @Prop({required:true})
+    homeAddress : string;
+
+    @Prop({ type: Date })
+    dob:Date;
+
+    @Prop()
+    group:string;
+
+    @Prop()
+    jobTitle:string;
+
+    @Prop()
+    company:string;
+
+    @Prop()
+    workAddress:string;
+
+    @Prop()
+    image:string;
+
+}
+
+export const ContactSchema = SchemaFactory.createForClass(Contact);
---
+++ backend/src/contacts/contacts.service.ts
@@ -0,0 +1,39 @@
+import { Injectable,NotFoundException } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Contact, ContactDocument } from './contact.schema';
+
+@Injectable()
+export class ContactsService {
+    constructor(
+        @InjectModel(Contact.name) private contactModel: Model<ContactDocument>
+    ) {}
+
+    async create(contact: Contact): Promise<Contact> {
+        const newContact = new this.contactModel(contact);
+        return newContact.save();
+    }
+
+    async findAll(): Promise<Contact[]> {
+        return this.contactModel.find().exec();
+    }
+
+    async findOne(id: string): Promise<Contact> {
+        const contact = await this.contactModel.findById(id).exec();
+        if (!contact) throw new NotFoundException('Contact not found');
+        return contact;
+    }
+
+    async update(id:string,updateData:Partial<Contact>): Promise<Contact> {
+        const updated = await this.contactModel.findByIdAndUpdate(id, updateData,{
+            new: true,
+        }).exec();
+        if(!updated) throw new NotFoundException('Contact not found');
+        return updated;
+    }
+
+    async remove(id: string): Promise<void> {
+        const result = await this.contactModel.findByIdAndDelete(id).exec();
+        if (!result) throw new NotFoundException('Contact not found');
+    }
+}
